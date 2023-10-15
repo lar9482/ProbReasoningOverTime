ae4cd81359da9a92b3a233499684f8d9ca7cfd66
larunnel161 <larunnel161@gmail.com>
2023-10-15 00:27:40 -0500

    Renaming directories to indicate that they had 1000 time steps

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from testHMM import testHMM, HMMEvidence
 
 from analyzeDBNData import analyzeTimeStepResults
 from multiprocessing import Process
-
+from CS5313_Localization_Env.localization_env import le
+from DBN.RobotDBN import RobotDBN
 def runHMMTests():
     testHMM(HMMEvidence.redEyes_sleepClass)
     testHMM(HMMEvidence.redEyes_notSleepClass)
@@ -39,7 +40,37 @@ def analyzeDBNTests():
         for testCase in testCases:
             analyzeTimeStepResults(testCase)
 
-            
+def runToyDBN():
+    dimensionX = 20
+    dimensionY = 20
+
+    action_bias = 0.5
+    observation_noise = 0.95
+    action_noise = 0.5
+    dimensions = (dimensionX, dimensionY)
+    seed = 768
+    (x, y) = (750, 750)
+    env = le.Environment(
+        action_bias, 
+        observation_noise, 
+        action_noise, 
+        dimensions, 
+        seed=seed, 
+        window_size=[x,y]
+    )
+    
+    DBN = RobotDBN(env, 1000)
+
+    observation = env.observe()
+    for t in range(1, 9999+1):
+        samples = DBN.runParticleFilter(observation)
+        (prob, mostLikelySamples) = DBN.getMostLikelySamples(samples)
+
+        locProbs = DBN.calcLocationProbsFromSamples(samples, dimensionX, dimensionY)
+        headingProbs = DBN.calcHeadingProbsFromSamples(samples)
+        env.update(locProbs, headingProbs)
+        observation = env.move()
+
 def main():
     dbnDataset = getTestDBNDatasetParameters()
     totalTimeSteps = 1000
@@ -47,7 +78,7 @@ def main():
     # runDBNTest(dbnDataset[10][0], totalTimeSteps)
     # analyzeTimeStepResults(dbnDataset[10][0])
     # runDBNTests()
-    analyzeDBNTests()
-
+    # analyzeDBNTests()
+    runToyDBN()
 if __name__ == "__main__":
     main()
